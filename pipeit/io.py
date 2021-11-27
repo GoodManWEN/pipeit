cb960f1d81d2efdd89e5d6cde491a49ac092cde9
GoodManWEN <44322449+GoodManWEN@users.noreply.github.com>
2021-11-27 22:10:25 +0000

    Create io.py

---
+++ pipeit/io.py
@@ -0,0 +1,9 @@
+def Read(file_name):
+    with open(file_name, 'r', encoding='utf-8') as f:
+        text = f.read()
+    return text
+
+def Write(file_name, text):
+    with open(file_name, 'r', encoding='utf-8') as f:
+        f.write(text)
+    return 
